20be8db2a6110f67493a349491030dbce13520b6
Divyanshi Patkar <divyanshipatkar339@gmail.com>
2026-03-14 17:56:02 +0530

    frontend work

---
+++ frontend/src/components/ProfileCard.jsx
@@ -0,0 +1,17 @@
+function ProfileCard() {
+  return (
+    <div className="profileCard">
+
+      <img src="https://i.pravatar.cc/100" alt="profile" />
+
+      <h3>Alex Morgan</h3>
+
+      <p>Content Creator</p>
+
+      <button>Copy Link</button>
+
+    </div>
+  );
+}
+
+export default ProfileCard;
