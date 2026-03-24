3504e76d357ba346c09310baa8b76c4b1253fd5c
Divyanshi Patkar <divyanshipatkar339@gmail.com>
2026-03-24 14:22:25 +0530

    landing page UI

--- frontend/src/pages/app.jsx
+++ frontend/src/pages/app.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import Landing from "./Landing";
 
-function App(){
-    return (
-        <Dashboard />
-    )
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
 
   return (
-    <Routes>
+    <>
+    
+
+     <Routes>
+      <Route path="/Landing" element={<Landing/>}/>
       <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Login setToken={setToken} />} />
       <Route path="/dashboard" element={token ? <Dashboard setToken={setToken} /> : <Navigate to="/" />} />
-    </Routes>
+    </Routes> 
+    </>
   );
 }
 export default App;
