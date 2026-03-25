e3edd7678c99171a16cf6033dd5c8823087b5759
Divyanshi Patkar <divyanshipatkar339@gmail.com>
2026-03-25 22:48:24 +0530

    ...

--- frontend/src/pages/app.jsx
+++ frontend/src/pages/app.jsx
@@ -14,7 +14,7 @@ function App() {
      <Routes>
       <Route path="/Landing" element={<Landing/>}/>
       <Route path="/Analytics" element={<Analytics/>}/>
-       <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Login setToken={setToken} />} /> 
+      <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Login setToken={setToken} />} /> 
       <Route path="/dashboard" element={token ? <Dashboard setToken={setToken} /> : <Navigate to="/" />} />
     </Routes> 
     </>
