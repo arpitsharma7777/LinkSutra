ed41fdc16a16f65a8e046693e6c120212d617416
ARPIT SHARMA <arpitsharma8975@gmail.com>
2026-03-28 10:29:32 +0530

    Revert "card typo fix"
    This reverts commit db00dbf1191e603525b522867a3d8b949975ec4c.

--- frontend/src/components/Feature.jsx
+++ frontend/src/components/Feature.jsx
@@ -5,22 +5,22 @@ function Features() {
 
       <div className="features-container">
         <div className="feature-card">
-          <h2>🔗 Unlimited Links</h2>
+          <h2>🔗 **Unlimited Links** </h2>
           <p>add, edit, reorder, and manage links freely</p>
         </div>
 
         <div className="feature-card">
-         <h2>📊 Privacy-First Analytics</h2>
+         <h2>📊 **Privacy-First Analytics**</h2>
           <p> server-side click tracking, zero cookies</p>
         </div>
 
         <div className="feature-card">
-           <h2>👤 Custom Profile</h2>
+           <h2>👤 **Custom Profile** </h2>
           <p>set profile as our environment </p>
         </div>
 
         <div className="feature-card">
-         <h2>📦 Full Data Export</h2>
+         <h2>📦 **Full Data Export**</h2>
           <p>download your entire profile as JSON anytime</p>
         </div>
       </div>
